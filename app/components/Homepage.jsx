a6b75574f61bd6932b037627e58e5f78c6206130
Aaryan Shaikh <aayanshaikh27638@gmail.com>
2023-07-16 01:07:56 +0530

    added image stack effect

--- app/components/Homepage.jsx
+++ app/components/Homepage.jsx
@@ -1,11 +1,15 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { MdOutlineKeyboardDoubleArrowRight } from 'react-icons/md'
 
 const Homepage = () => {
+    const [isColor, setisColor] = useState(false)
+    console.log(isColor);
+
     return (
         <section className="home section grid">
-            <img className='home__img' src='../assets/home.png' />
+            <img className='home__img' src='../assets/home.png' style={{ filter: `saturate(${isColor ? "1" : "0"})`, transition: ".3s ease-in-out" }} />
 
             <div className="home__content">
                 <div className="home__data">
@@ -14,7 +18,7 @@ const Homepage = () => {
                         I am a web development wizard specializing in React and Next.js. With my spellbinding skills, I craft dynamic and responsive user interfaces that captivate and engage. My potions blend creativity, functionality, and performance to create enchanting web experiences. Join me on this magical journey as we unlock the secrets of React and Next.js!
                     </p>
 
-                    <Link href="/about" className='button'>More About me <span className='button__icon'><MdOutlineKeyboardDoubleArrowRight /></span></Link>
+                    <Link onMouseOver={() => { setisColor(true) }} onMouseOut={() => { setisColor(false) }} href="/about" className='button'>More About me <span className='button__icon'><MdOutlineKeyboardDoubleArrowRight /></span></Link>
                 </div>
             </div>
 
--- app/components/ImageStack.jsx
+++ app/components/ImageStack.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const ImageStack = () => {
   return (
-    <div>ImageStack</div>
+    <div className='card-group'>
+      <div className="big-card imgCard"></div>
+      <div className="big-card imgCard"></div>
+      <div className="big-card imgCard"></div>
+      <div className="big-card imgCard"></div>
+      <div className="little-card imgCard"></div>
+      <div className="little-card imgCard"></div>
+      <div className="little-card imgCard"></div>
+      <div className="little-card imgCard"></div>
+    </div>
   )
 }
 
