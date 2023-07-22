d961c7076321f54a5f11c57fe9ebb761f5467a57
Aaryan Shaikh <aayanshaikh27638@gmail.com>
2023-07-22 13:26:28 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -13,8 +13,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ReduxProvider><IntroTracker /></ReduxProvider>
+      <ReduxProvider><IntroTracker /></ReduxProvider>
+      <body className={inter.className} >
         <ReduxProvider>
           {children}
         </ReduxProvider>
